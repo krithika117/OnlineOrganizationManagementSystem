7c27fe0906968bf6fab0cbfb8287990cffe60b0f
krithika117 <krithikanithyanandam1@gmail.com>
2023-04-29 23:55:32 +0530

    calendar model changes for meetings

--- OnlineOrganizationManagementSystem/Models/CalendarEvent.cs
+++ OnlineOrganizationManagementSystem/Models/CalendarEvent.cs
@@ -9,6 +9,8 @@ namespace OnlineOrganizationManagementSystem.Models
 
         [Required]
         public string Title { get; set; }
+        public string? Details { get; set; }
+        public string? Scope { get; set; }
 
         [Required]
         public DateTime Date { get; set; }
